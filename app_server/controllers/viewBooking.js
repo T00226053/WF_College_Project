2ddc29e437a81bb6a8965dc47bde1200f66c636b
T00226053 <liam.oconnor-collum@students.ittralee.ie>
2023-10-16 00:41:14 +0100

    Changed CSS

--- app_server/controllers/viewBooking.js
+++ app_server/controllers/viewBooking.js
@@ -2,11 +2,11 @@
 const viewBooking = function (req, res) {
 	res.render('viewBooking', {
 		title: 'View Bookings' ,
-		rows: [{
+		bookings: [{
 			id: 2,
 			memberName: 'John',
 			className: 'Pilates',
-			classLength: 2,
+			classLength: 1.5,
 			date: '10/10/2023',
 			time: '8:00'
 		}]
